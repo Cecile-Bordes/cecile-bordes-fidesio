a2b2a1562fc36b7ab7b3c6466e82559088f17edd
Cecile Bordes <bordes_cecile@hotmail.com>
2021-09-05 23:00:52 +0200

    ajout grille sass

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/sass/app.scss'
 
 Vue.config.productionTip = false
